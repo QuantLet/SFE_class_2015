a89f808cece1cb9bdf03c51a4c50fa9b388ac713
AlexH20 <97352766+AlexH20@users.noreply.github.com>
2022-07-01 10:59:53 +0200

    Update SFErealizedVolatilityDAX.R

--- SFErealizedVolatilityDAX/SFErealizedVolatilityDAX.R
+++ SFErealizedVolatilityDAX/SFErealizedVolatilityDAX.R
@@ -1,5 +1,6 @@
-# Dax realized variance, observed and predicted volatility and price movements
 # Install Performance Analytics package version 1.4.3541 and highfrequency package version 0.4. Zoo and xts package needs to be installed
+# ipc realized variance, observed and predicted volatility and price
+# movements
 
 # Clear enviorenment
 graphics.off()
@@ -9,48 +10,48 @@ rm(list = ls())
 library(highfrequency)
 library(PerformanceAnalytics)
 
-# Download File
-dax = read.csv(file = "Dax Data current.csv", sep = ",", head = TRUE, na.strings = c("", 
-                                                                                     "NA"))
+# Download file
+ipc = read.csv(file = "IPC RV Data.csv", sep = ",", head = TRUE, na.strings = c("", 
+                                                                                "NA"))
 
 # Download data and transform it to a data frame
-dax = as.data.frame(dax)
+ipc = as.data.frame(ipc)
 
 # Select my working vextors for 5 minutes daily realized variance
-daxrv = dax$Realized.Variance..5.minute.
+ipcrv = ipc$Realized.Variance..5.minute.
 
 # Convert the date strings to charachter vectors in order to transform
 # it to Date format.
-dax[, 1] = as.character(dax[, 1])
-daxt     = dax$DateID
+ipc[, 1] = as.character(ipc[, 1])
+ipct     = ipc$DateID
 
 # Transform the time and date charachter vectors to a valid R format
-time = as.POSIXct(paste(daxt), format = "%Y-%m-%d")
+time = as.POSIXct(paste(ipct), format = "%Y-%m-%d")
 
 # set our xts elements Realized volatility 5 min
-daxrv = xts(daxrv, order.by = time)
+ipcrv = xts(ipcrv, order.by = time)
 
 # Returns 5 min
-returns = xts(dax[, 4], order.by = time)
+returns = xts(ipc[, 4], order.by = time)
 
 # 5 minutes with one minute subsampling
-daxrvs = xts(dax[, 5], order.by = time)
+ipcrvs = xts(ipc[, 5], order.by = time)
 
 # Eliminate NA's
-daxrv  = daxrv[complete.cases(daxrv)]
-daxrvs = daxrvs[complete.cases(daxrvs)]
+ipcrv  = ipcrv[complete.cases(ipcrv)]
+ipcrvs = ipcrvs[complete.cases(ipcrvs)]
 
 # Apply har model
-daxm = harModel(data = daxrv, periods = c(1, 5, 22), RVest = c("rCov"), 
+ipcm = harModel(data = ipcrv, periods = c(1, 5, 22), RVest = c("rCov"), 
                 type = "HARRV", h = 1, transform = NULL)
 
-daxm = cbind(daxm$fitted.values,daxrv)
+ipcm = cbind(ipcm$fitted.values,ipcrv)
 
-# Plot observed and forecasted volatility
-pdf(file = "DAX1.pdf", width = 12, height = 4)
+# Plot observed and forecasted volatility for the given time frame
+pdf(file = "IPC1.pdf", width = 12, height = 4)
 
 chart.TimeSeries(
-  daxm,
+  ipcm,
   type = "l", 
   main = "Observed and forecasted RV based on HAR Model: HARRV", 
   ylab = "Realized Volatility",
@@ -68,39 +69,40 @@ cycles.dates = list(c("2000-01-03", "2002-01-03"), c("2008-09-01", "2010-09-01")
 risk.dates = c("2000-01-04", "2008-09-01", "2015-06-03")
 risk.labels = c("Dot-com bubble", "Global Crisis", "Commodities volatility")
 
-chart.TimeSeries(daxrv, type = "l", main = "Dax Volatility", ylab = "", period.areas = cycles.dates, period.color = "#0000FF22", 
-                 event.lines = risk.dates, event.labels = risk.labels, event.color = "red", 
-                 lwd = 1, grid.color = NA)
-
+chart.TimeSeries(ipcrv, type = "l", main = "IPC volatility", ylab = "Return", 
+                 col = "black", grid.color = "yellow", period.areas = cycles.dates, 
+                 period.color =  "#0000FF22", event.lines = risk.dates, event.labels = risk.labels, 
+                 event.color = "red", lwd = 1)
 
 dev.off()
 
 # plot returns
-pdf(file = "DAX2.pdf", width = 12, height = 4)
+pdf(file = "IPC2.pdf", width = 12, height = 4)
 
-chart.TimeSeries(returns, type = "l", main = "DAX returns", ylab = "Return", 
-                 period.areas = cycles.dates, period.color = "#0000FF22", event.lines = risk.dates, 
-                 event.labels = risk.labels, event.color = "red", lwd = 1)
+chart.TimeSeries(returns, type = "l", main = "IPC returns", ylab = "Return", 
+                 col = "black", grid.color = "yellow", period.areas = cycles.dates, 
+                 period.color = "#0000FF22", event.lines = risk.dates, event.labels = risk.labels, 
+                 event.color = "red", lwd = 1)
 
 dev.off()
 
 # 2008 Volatility
-daxrv8 = daxrv["2008"]
+ipcrv8 = ipcrv["2008"]
 
 # 2008 one minute lagged variance
-daxrvs8 = daxrvs["2008"]
+ipcrvs8 = ipcrvs["2008"]
 
 # apply har model
-daxm8 = harModel(data = daxrv8, periods = c(1, 5, 22), RVest = c("rCov"), 
+ipcm8 = harModel(data = ipcrv8, periods = c(1, 5, 22), RVest = c("rCov"), 
                  type = "HARRV", h = 1, transform = NULL)
 
-daxm8 = cbind(daxm8$fitted.values,daxrv8)
+ipcm8 = cbind(ipcm8$fitted.values,ipcrv8)
 
 # Plot harModel 2008
-pdf(file = "DAX3.pdf", width = 12, height = 4)
+pdf(file = "IPC3.pdf", width = 12, height = 4)
 
 chart.TimeSeries(
-  daxm8,
+  ipcm8,
   type = "l", 
   main = "Observed and forecasted RV based on HAR Model: HARRV", 
   ylab = "Realized Volatility",
@@ -114,24 +116,23 @@ chart.TimeSeries(
 )
 
 dev.off()
-
 # Plot realized variance and a volatility approx.
 pdf(file = "DAX4.pdf", width = 10, height = 12)
 
 par(mfrow = c(3, 1))
 
-chart.TimeSeries(daxrv8, colorset = "red", type = "h", main = "5 minutes realized volatility", 
+chart.TimeSeries(ipcrv8, col = "red", type = "h", main = "5 minutes realized volatility", 
                  ylab = "RV", xlab = "Time")
 
 # Plot realized volatility with subsampling
-chart.TimeSeries(daxrvs8, colorset = "blue", type = "h", main = " 5 minutes realized volatility with subsampling", 
+chart.TimeSeries(ipcrvs8, col = "blue", type = "h", main = " 5 minutes realized volatility with subsampling", 
                  ylab = "RV", xlab = "Time")
 
 # set difference between volatilities with and wothout subsampling.
-diff = daxrv8 - daxrvs8
+diff = ipcrv8 - ipcrvs8
 
 # Plot the volatility differences
-chart.TimeSeries(diff, colorset = "purple", type = "h", main = " Difference in lagged and normal volatility", 
+chart.TimeSeries(diff, col = "purple", type = "h", main = " Difference in lagged and normal volatility measures", 
                  ylab = "RV", xlab = "Time")
 
 dev.off()
